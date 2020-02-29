195c4d8a141c012443699985c49f9e958571a76f
Alejandro Franco <jafrancov@gmail.com>
2020-02-29 11:31:20 -0600

    Hello SwiftUI

--- Hello SwiftUI/ContentView.swift
+++ Hello SwiftUI/ContentView.swift
@@ -9,8 +9,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    @State private var tapCount = 0
+    @State private var username = ""
+    let students = ["Juan", "Pedro", "Luisa"]
+    @State private var selectedStudent = 0
+    
     var body: some View {
-        Text("Hello, World!")
+//        Form{
+//            Text("Username")
+//                .font(.callout)
+//                .bold()
+//            TextField("Enter username...", text: $username)
+////                .textFieldStyle(RoundedBorderTextFieldStyle())
+//            Text("Your username is \(username)")
+//        }
+        
+//        NavigationView {
+//            Form {
+//                Picker("Select your student", selection: $selectedStudent) {
+//                    ForEach(0 ..< students.count, id: \.self) {
+//                        Text(self.students[$0])
+//                    }
+//                }
+//                Text("Student selected: \(students[self.selectedStudent])")
+//            }
+//        }
+        NavigationView {
+            Form {
+                Section {
+                    Picker(selection: $selectedStudent, label: Text("Student")) {
+                        ForEach(0 ..< students.count) {
+                            Text(self.students[$0])
+
+                        }
+                    }
+                }
+            }.navigationBarTitle("Select your student")
+
+        }
+        
+//        Form {
+//        Button("Tap to count: \(tapCount)") {
+//            self.tapCount += 1
+//        }
+        
+//        NavigationView {
+//            Form {
+//                Section {
+//                    Text("Alejandro Franco")
+//                        .font(.title)
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                        .font(.title)
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                        .font(.title)
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                        .font(.title)
+//                }
+//                Section {
+//                    Text("Alejandro Franco")
+//                        .font(.title)
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                        .font(.title)
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                        .font(.title)
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                    Text("Alejandro Franco")
+//                        .font(.title)
+//                }
+//            } .navigationBarTitle("SwiftUI")
+//        }
     }
 }
 
